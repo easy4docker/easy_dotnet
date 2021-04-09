addce522a76c7c9eb5c344e2e4783582a46944c1
easy4docker <easy4docker@gmail.com>
2021-04-09 12:16:02 -0700

    update

--- aspnetapp/wwwroot/app/demoApp.js
+++ aspnetapp/wwwroot/app/demoApp.js
@@ -1,29 +1,43 @@
 
-angular.module('demoApp', [])
-.directive('appHeader', function() {
+const app = angular.module('demoApp', [])
+app.directive('appHeader', function() {
   return {
-    templateUrl: "/app/templates/header.html",
+    templateUrl: '/app/templates/header.html?' + new Date().getTime(),
     replace: true
   };
-}).directive('appBody', function() {
+})
+app.directive('appBody', function() {
   return {
-    templateUrl: '/app/templates/body/bodyRoot.html',
+    templateUrl: '/app/templates/body/bodyRoot.html?' + new Date().getTime(),
+    controller: function AppBodyController() {
+      this.switchModule = function() {
+        alert('switchModule')
+      };
+    },
     replace: true
   };
 }).directive('appBodyApi', function() {
   return {
-    templateUrl: '/app/templates/body/bodyApi.html',
+    templateUrl: '/app/templates/body/bodyApi.html?' + new Date().getTime(),
     replace: true
   };
 }).directive('appBodyDocument', function() {
   return {
-    templateUrl: '/app/templates/body/bodyDocument.html',
+    templateUrl: '/app/templates/body/bodyDocument.html?' + new Date().getTime(),
     replace: true
   };
-}).controller('loadData', function($scope, $http) {
-  $scope, $http
+})
+app.controller('loadData', function($scope, $http) {
+
   $http.get("/api")
   .then(function(response) {
     $scope.data = response.data;
   });
+});
+
+app.controller('appBody', function($scope, $http) {
+  $scope.bodyModule = 'api';
+  $scope.switchModule = function(v) {
+    $scope.bodyModule=v;
+  };
 });
